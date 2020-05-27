6c435048f11a6aa2132bc02877ff7e5dc50ee2d6
vilarj <lamaravillasan@outlook.com>
2020-05-27 15:07:02 -0400

    Simple server to client app in python
    Signed-off-by: vilarj <lamaravillasan@outlook.com>

--- src/Server.py
+++ src/Server.py
@@ -1,10 +1,12 @@
 import socket
 
-skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP Protocol
+skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP Protocol
 skt.bind((socket.gethostname(), 1234))
 skt.listen(5)
 
 while True:
-    clt, address = skt.accept()
-    print(f"{address} just connected")
-    clt.send(bytes("Socket Programming in Python", "UTF-8"))
+    communicate, address = skt.accept()
+    print(f"{address} just connected✔")
+    communicate.send(bytes("Socket Programming in Python", "UTF-8"))
+
+    communicate.close()
